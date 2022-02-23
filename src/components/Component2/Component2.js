4e79b79aa48ef0fbd228e8f07eb403ac6d1cea4f
PrajaktaChandilkar <prajakta0704@gmail.com>
2022-02-23 17:08:02 +0530

    changes

---
+++ src/components/Component2/Component2.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Component2() {
+  return (
+    <span className="Component2">
+      <h1>lorem</h1>
+      <p>
+        omnis, fugiat excepturi est ab nemo eaque a distinctio laudantium iure
+        corporis. Soluta, rem! Corporis et accusamus voluptas ipsum quibusdam a
+        doloribus est. Dignissimos minus earum excepturi harum eius sed
+        laudantium iste voluptatem amet ullam tempore voluptates esse
+        praesentium animi vel corrupti itaque incidunt illum, error quod
+        nesciunt. Tenetur molestiae quis, minima nihil rem nulla ea quae
+        repellendus necessitatibus maiores assumenda debitis obcaecati error,
+        veniam corrupti dolor rerum inventore doloremque itaque blanditiis
+        molestias eum tempore? Quo, voluptas impedit?
+      </p>
+    </span>
+  );
+}
+
+export default Component2
